8eb1b8a0bc2c6792118998cb7db5b4e7355ef988
FeherBalazs <feher.balazs.feher@gmail.com>
2016-08-05 10:31:02 +0100

    feat: Added program for non_stop image collection
    This program captures images for the non-stop category on the raspberry pi.

---
+++ camera_non_stop_image_collect.py
@@ -0,0 +1,20 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+# Rotation
+camera.rotation = 180
+
+# Test View
+#camera.start_preview()
+#sleep(10)
+#camera.stop_preview()
+
+# Capture test_image
+for i in range(400):
+	camera.start_preview()
+	sleep(0.05)
+	camera.resolution = (320, 240)
+	camera.capture('/home/pi/Documents/Train/data/non_stop_'+str(i)+'.jpg')
+	camera.stop_preview()
